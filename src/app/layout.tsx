83ec39d9454ef8e46ed5222856734c535f2dc537
Khyle Matthew P. Yanes <yaneskhylematthew02@gmail.com>
2024-06-25 22:43:51 +0800

    Adding home ui elements

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "../components/header";
+import Footer from "@/components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Header></Header>
         <div className=" mr-5 ml-5">{children}</div>
+        <Footer />
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
-
 import Hero from "./(pages)/home/page";
 
 export default function Home() {
   return (
-    <main className=" min-h-[200rem]">
-      <Hero />
+    <main className="">
+      <div className="p-5">
+        <Hero />
+      </div>
     </main>
   );
 }
